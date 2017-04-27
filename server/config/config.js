0fbfd6fadb42973291b082bf0653488d6ad6defe
nicole <nodejs@gmx.de>
2017-04-27 14:46:38 +0200

    fixed port

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,5 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else {
-  process.env.PORT = 80;
   process.env.MONGODB_URI = 'mongodb://todo-app-user:todo-app-pw@ds151060.mlab.com:51060/todo-app-api';
 }
