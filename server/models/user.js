d01b0c5403f569c954662dc65336c135a09c3ee8
nicole <nodejs@gmx.de>
2017-04-28 21:16:21 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,6 +71,29 @@ UserSchema.statics.findByToken = function (token) {
   })
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+
+    if (!user) {
+      return  Promise.reject();
+    }
+    // return Promise.resolve(user);
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // console.log('user: ' + JSON.stringify(user));
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
--- server/server.js
+++ server/server.js
@@ -109,9 +109,21 @@ app.post('/users', (req, res) => {
   });
 });
 
-
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  var result = User.findByCredentials(body.email, body.password);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
